e812b0fbfdb176b0324072206f9955bc8ae7d621
Frenkieli <49364993+Frenkiele@users.noreply.github.com>
2020-08-13 15:20:34 +0800

    feat: 追加使用者條款
    將使用者條款放進去，這樣可以減少爭議

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -7,9 +7,13 @@
 import express from 'express';
 const router = express.Router();
 
+let title = 'DonHand - 助手(驢驢)';
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'linebot_crawler'});
+  res.render('index', { title: title});
+});
+router.get('/TermsOfUse', (req, res) => {
+  res.render('TermsOfUse', { title: title});
 });
 
 export default router;
