578ccdfdcb709a8146af9d97491a760a8f29490c
Frenkieli <49364993+Frenkiele@users.noreply.github.com>
2020-09-04 16:50:08 +0800

    fix: 不應該限制寬度
    通常來說1920和1336就可以了應該其他的不用過度在意

--- src/client/app.vue
+++ src/client/app.vue
@@ -18,8 +18,6 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;700&display=swap');
 body{
-  margin: auto;
-  max-width: 1920px;
   font-family: 'Noto Sans TC', sans-serif;
 }
 .displaynone{
