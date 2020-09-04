32a9b49e291cf2d02b402c8513da7fd3e6279b94
Frenkieli <49364993+Frenkiele@users.noreply.github.com>
2020-09-04 14:00:32 +0800

    feat: 製作navbar電腦版畫面
    使用遞迴的方式快速產生對應畫面，同時保留原始html做未來參考
    
      - src\client\views\headerItem.vue css所在控制所有的ul>li>ul<li的動作，保留原始html方便未來修正
      - src\client\views\components\headerNavbarList.vue 運用遞迴方式生產好幾層的下拉式選單方便使用

---
+++ src/client/views/components/headerNavbarList.vue
@@ -0,0 +1,33 @@
+<!--
+  /**
+  * @description header和導覽列內的下拉式選單
+  * @author frenkie
+  * @date 2020-09-04
+  */
+-->
+<template>
+  <ul class="header_navbar_list">
+    <li
+      v-for="value in navList"
+      class="header_navbar_list_item"
+      :key="value.name"
+    >
+      <span class="header_navbar_list_item_span">{{ value.name }}</span>
+      <headerNavbarList
+        :navList="value.navList"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'headerNavbarList',
+  props:{
+    navList: {
+      type: Array,
+      default: []
+    }
+  }
+}
+</script>
--- src/client/views/headerItem.vue
+++ src/client/views/headerItem.vue
@@ -15,9 +15,12 @@
       <figcaption class="displaynone">DonHand - 助手(驢驢)，一個擅長傳梗圖的linebot機器人</figcaption>
     </figure>
     <nav class="header_navbar">
-      <ul class="header_navbar_list">
+      <headerNavbarList
+        :navList="navList"
+      />
+      <!-- <ul class="header_navbar_list">
         <li class="header_navbar_list_item">
-          <span class="header_navbar_list_item_span">介紹</span>
+          <span class="header_navbar_list_item_span">基本介紹</span>
           <ul class="header_navbar_list">
             <li class="header_navbar_list_item">
               <span class="header_navbar_list_item_span">作者</span>
@@ -28,16 +31,25 @@
           </ul>
         </li>
         <li class="header_navbar_list_item">
-          <span class="header_navbar_list_item_span">試用</span>
+          <span class="header_navbar_list_item_span">試用功能</span>
         </li>
         <li class="header_navbar_list_item">
-          <span class="header_navbar_list_item_span">學習參考</span>
           <ul class="header_navbar_list">
             <li class="header_navbar_list_item">
               <span class="header_navbar_list_item_span">typescript</span>
               <ul class="header_navbar_list">
                 <li class="header_navbar_list_item">
                   <span class="header_navbar_list_item_span">立即前往</span>
+                  <ul class="header_navbar_list">
+                    <li class="header_navbar_list_item">
+                      <span class="header_navbar_list_item_span">立即前往2</span>
+                      <ul class="header_navbar_list">
+                        <li class="header_navbar_list_item">
+                          <span class="header_navbar_list_item_span">立即前往3</span>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
                 </li>
               </ul>
             </li>
@@ -45,19 +57,84 @@
               <span class="header_navbar_list_item_span">express</span>
             </li>
           </ul>
+          <span class="header_navbar_list_item_span">學習參考</span>
         </li>
         <li class="header_navbar_list_item">
           <span class="header_navbar_list_item_span">使用條款</span>
         </li>
-      </ul>
+      </ul> -->
     </nav>
   </header>
 </template>
 
 <script>
+import headerNavbarList from '@@/views/components/headerNavbarList';
 export default {
+  components:{
+    headerNavbarList
+  },
   setup() {
-    return {};
+    const navList = [
+      {
+        name: "基本介紹",
+        link: '/',
+        navList: [
+          {
+            name: "作者",
+            link: '/#author'
+          },
+          {
+            name: "理念",
+            link: '/#idea'
+          },
+        ],
+      },
+      {
+        name: "試用功能",
+        link: '/chatroom'
+      },
+      {
+        name: "學習參考",
+        link: '/reference',
+        navList: [
+          {
+            name: "typescript",
+            link: '/reference#typescript',
+            navList: [
+              {
+                name: "立即前往",
+                link: 'https://www.google.com/',
+                navList: [
+                  {
+                    name: "立即前往2",
+                    link: 'https://www.google.com/',
+                    navList: [
+                      {
+                        name: "立即前往3",
+                        link: 'https://www.google.com/',
+                        navList: [{}],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "express",
+            link: '/reference#express',
+          },
+        ],
+      },
+      {
+        name: "使用條款",
+        link: '/TermsOfUse',
+      },
+    ];
+
+    return {
+      navList
+    };
   },
 };
 </script>
@@ -72,30 +149,72 @@ export default {
   &_logo {
     position: absolute;
     &_img {
-      height: 60px;
+      height: 40px;
       vertical-align: middle;
     }
   }
-  &_navbar {
+  ::v-deep(.header_navbar) {
     &_list {
       text-align: right;
       &_item {
+        transition: width 0.3s;
         position: relative;
         display: inline-block;
-        width: 150px;
+        width: 100px;
         margin-left: 20px;
         padding: 5px;
         box-sizing: border-box;
-        border-right: 1px solid #f00;
-        .header_navbar_list{
-          position: absolute;
-          right: -2px;
-          border-right: 1px solid #0f0;
-        }
         &_span {
-          height: 60px;
+          z-index: 1;
+          position: relative;
           box-sizing: border-box;
-          line-height: 60px;
+          height: 40px;
+          line-height: 40px;
+          white-space: nowrap;
+        }
+        .header_navbar_list {
+          transition: 0.3s;
+          position: absolute;
+          top: 0%;
+          right: 0;
+          .header_navbar_list {
+            transition: 0.3s;
+            width: 0;
+            &_item {
+              display: block;
+              width: 0;
+              padding: 0;
+              margin-left: 0;
+              &_span {
+                transition: 0.3s;
+                display: inline-block;
+                width: 0;
+                height: 0;
+                overflow: hidden;
+              }
+            }
+          }
+        }
+        &:hover {
+          > .header_navbar_list {
+            top: 100%;
+            > .header_navbar_list_item {
+              width: 100px;
+              &:hover {
+                > .header_navbar_list {
+                  top: 18px;
+                  right: 100%;
+                  width: 100px;
+                }
+              }
+              > .header_navbar_list_item_span {
+                height: 40px;
+                width: 100%;
+                padding: 5px;
+                line-height: 30px;
+              }
+            }
+          }
         }
       }
     }
